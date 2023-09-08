c1dd9b35179318c084289187a4a9cf3f23b075ed
stempelo <matteo.boffo@gmail.com>
2023-09-08 13:17:28 +0000

    wi

---
+++ trainData.py
@@ -0,0 +1,12 @@
+TrainDataPhrases = {
+    '1'   : 'La programmazione è l\'arte di creare istruzioni che i computer seguono per risolvere problemi complessi.',
+    '2'   : 'L\'intelligenza artificiale sta rivoluzionando settori come la sanità e l\'automazione industriale.',
+    '3'   : 'La sicurezza informatica è fondamentale per proteggere dati sensibili da minacce online.',
+    '4'   : 'Il cloud computing ha reso più accessibili risorse di calcolo e archiviazione a livello globale.',
+    '5'   : 'I linguaggi di programmazione come Python e JavaScript sono ampiamente utilizzati nello sviluppo software.',
+    '6'   : 'La realtà virtuale e aumentata stanno cambiando la forma delle esperienze utente e dell\'addestramento.',
+    '7'   : 'La crittografia è alla base delle comunicazioni sicure su Internet e delle transazioni finanziarie.',
+    '8'   : 'I robot autonomi e i veicoli senza conducente stanno trasformando il settore dei trasporti.',
+    '9'   : 'La blockchain è la tecnologia dietro le criptovalute come il Bitcoin e promette trasparenza e sicurezza.',
+    '10'  : 'Il machine learning permette alle macchine di apprendere dai dati e di prendere decisioni basate su pattern.',
+}
