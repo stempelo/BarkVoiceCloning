53293e2a0225089702aaf19e8b1ca78652ab8c0c
stempelo <matteo.boffo@gmail.com>
2023-09-14 15:10:29 +0000

    create_data.py

--- create_data.py
+++ create_data.py
@@ -8,24 +8,43 @@ from bark import text_to_semantic
 from bark.generation import load_model
 
 from data import load_books, random_split_chunk
+from IPython.lib.display import isdir
 
-loaded_data = load_books()
+#Parameters
+pathDataDrive = '/content/drive/MyDrive/ColabSpace/VoiceCloning/DataVoiceClone'
+extensionFile = '.txt'
+output = 'output'
 
 print('Loading semantics model')
 load_model(use_gpu=True, use_small=False, force_reload=False, model_type='text')
 
-output = 'output'
+
+
+if os.path.isdir(pathDataDrive):
+  print("Path exist on Drive")
+
 
 if not os.path.isdir(output):
-    os.mkdir(output)
+  os.mkdir(output)
 
-while 1:
-    filename = uuid.uuid4().hex + '.npy'
+contenuto_directory = os.listdir(pathDataDrive)
+file_con_estensione_desiderata = [file for file in contenuto_directory if file.endswith(extensionFile)]
+print(file_con_estensione_desiderata)
+
+for file in file_con_estensione_desiderata:
+    print(os.path.join(pathDataDrive, file))  # Stampa il percorso completo del file
+    filetxt = os.path.join(pathDataDrive, file)
+    filename = file[:-4] + '.npy'
+    print(filename)
     file_name = os.path.join(output, filename)
-    text = ''
-    while not len(text) > 0:
-        text = random_split_chunk(loaded_data)  # Obtain a short chunk of text
-        text = text.strip()
-    print('Generating semantics for text:', text)
+    print(file_name)
+    with open(filetxt, 'r') as file:
+        # Read the entire file content
+        content = file.read()
+        text = content.strip()
+        print("File content:")
+        print(text)
+    print('Generating semantics for text...')
     semantics = text_to_semantic(text, temp=round(random.uniform(0.6, 0.8), ndigits=2))
     numpy.save(file_name, semantics)
+
